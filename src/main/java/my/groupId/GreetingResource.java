937e145b0edccfcb9f3d1180d16d41c4d5ee526b
fiorenzino <fiorenzino@gmail.com>
2023-11-12 21:54:12 +0100

    vai

---
+++ src/main/java/my/groupId/GreetingResource.java
@@ -0,0 +1,61 @@
+package my.groupId;
+
+import io.quarkus.logging.Log;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import static java.lang.StringTemplate.RAW;
+import static java.util.FormatProcessor.FMT;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @Inject
+    DataSource dataSource;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        var world = "flower";
+        var age = 48;
+        return STR. """
+            Hello \{ world }! an older \{ age } man!
+        """ ;
+    }
+
+    @Path("/sql")
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sql() {
+        var i = 0;
+        String s = FMT. "The answer is %5d\{ i }";
+        System.out.println(s);
+        StringBuffer stringBuffer = new StringBuffer();
+        try (var conn = dataSource.getConnection()) {
+            var name = "TABLE_CONSTRAINTS";
+            var SQL = new QueryBuilder(conn);
+            var ps = SQL. "SELECT * FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = \{ name }" ;
+            var rs = ps.executeQuery();
+            var rsMetaData = rs.getMetaData();
+            while (rs.next()) {
+                stringBuffer.append(rsMetaData.getColumnName(1) + ": " + rs.getString(1) + "<br/>");
+                stringBuffer.append(rsMetaData.getColumnName(2) + ": " + rs.getString(2) + "<br/>");
+                stringBuffer.append(rsMetaData.getColumnName(3) + ": " + rs.getString(3) + "<br/>");
+            }
+            ps.close();
+            rs.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return stringBuffer.toString();
+    }
+}
---
+++ src/main/java/my/groupId/QueryBuilder.java
@@ -0,0 +1,36 @@
+package my.groupId;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class QueryBuilder implements StringTemplate.Processor<PreparedStatement, SQLException> {
+
+    Connection conn;
+
+    public QueryBuilder(Connection conn) {
+        this.conn = conn;
+    }
+
+    public PreparedStatement process(StringTemplate st) throws SQLException {
+        // 1. Replace StringTemplate placeholders with PreparedStatement placeholders
+        String query = String.join("?", st.fragments());
+
+        // 2. Create the PreparedStatement on the connection
+        PreparedStatement ps = conn.prepareStatement(query);
+
+        // 3. Set parameters of the PreparedStatement
+        int index = 1;
+        for (Object value : st.values()) {
+            switch (value) {
+                case Integer i -> ps.setInt(index++, i);
+                case Float f -> ps.setFloat(index++, f);
+                case Double d -> ps.setDouble(index++, d);
+                case Boolean b -> ps.setBoolean(index++, b);
+                default -> ps.setString(index++, String.valueOf(value));
+            }
+        }
+
+        return ps;
+    }
+}
